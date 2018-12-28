48c4dbc3bc21dc1f7f39550c6d462ef46351e942
ashishjunier <sanjay.wadhwani.1939>
2018-12-28 15:35:54 +0530

    change footer model popup design

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -4,11 +4,7 @@ import { RouterModule } from '@angular/router';
 import { VerticalLayout1Module } from 'app/layout/vertical/layout-1/layout-1.module';
 import { VerticalLayout2Module } from 'app/layout/vertical/layout-2/layout-2.module';
 import { VerticalLayout3Module } from 'app/layout/vertical/layout-3/layout-3.module';
-
 import { HorizontalLayout1Module } from 'app/layout/horizontal/layout-1/layout-1.module';
-
-
-
 
 @NgModule({
     imports: [
@@ -16,7 +12,6 @@ import { HorizontalLayout1Module } from 'app/layout/horizontal/layout-1/layout-1
         VerticalLayout1Module,
         VerticalLayout2Module,
         VerticalLayout3Module,
-
         HorizontalLayout1Module
     
     ],
